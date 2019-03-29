16d4d34429fbc5c01081f79ab2dfc338e61c5bf7
Matt Jones <Matt.Jones@1ststop.co.uk>
2019-03-29 15:35:13 +0000

    data table added, data from state rendering. Search functionality included

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,25 @@
                     <v-tooltip right>
                         <span>Source</span>
                     </v-tooltip>
-             {{ this.hotels }}
+
+                    <v-card>
+                        <v-card-title>
+                            Hotels
+                            <v-spacer></v-spacer>
+                            <v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details></v-text-field>
+                        </v-card-title>
+                        <v-data-table :headers="headers" :items="this.hotels" :search="search">
+                            <template v-slot:items="props">
+                                <td>{{ props.item.name }}</td>
+                                <td class="text-xs-right">{{ props.item.starRating }}</td>
+                                <td class="text-xs-right" v-for="item in props.item.facilities" :key ="item">{{ item  + ' ,' }}</td>
+                            </template>
+                            <v-alert v-slot:no-results :value="true" color="error" icon="warning">
+                                Your search for "{{ search }}" found no results.
+                            </v-alert>
+                        </v-data-table>
+                    </v-card>
+
                 </v-flex>
             </v-layout>
         </v-container>
@@ -37,13 +55,32 @@
 import BottomNav from './BottomNav.vue'
 import state from "./store.js";
 
-
 export default {
     components: {
         BottomNav
     },
     data: () => ({
-        drawer: null
+        drawer: null,
+        search: '',
+        headers: [{
+                text: 'Hotel Name',
+                align: 'left',
+                sortable: false,
+                value: 'name'
+            },
+            {
+                text: 'Star rating',
+                value: 'starRating',
+                                align: 'left',
+
+            },
+            {
+                text: 'Facilities',
+                value: 'facilities',
+                                align: 'left',
+
+            }
+        ],
     }),
     props: {
         source: String
@@ -55,8 +92,8 @@ export default {
         },
     },
 
-    created () {
-      console.log(this.$store.state.hotels)
+    created() {
+        console.log(this.$store.state.hotels)
     }
 }
 </script>
